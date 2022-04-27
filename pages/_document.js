0adc66e93fd9dee86e244c9ae006671c3c69aa9b
Thosam1 <thosamtenzin2002@gmail.com>
2022-04-28 00:20:39 +0200

    no more next-google-fonts and in _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,7 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
 // google fonts
-import { GoogleFonts } from 'next-google-fonts'
+// import { GoogleFonts } from 'next-google-fonts'
 
 // chakra ui - specific
 import { ColorModeScript } from "@chakra-ui/react"
@@ -10,7 +10,7 @@ import { ColorModeScript } from "@chakra-ui/react"
 export default function Document() {
   return (
     <Html lang="en">
-        <GoogleFonts href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;800&display=swap" />
+        {/* <GoogleFonts href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;800&display=swap" /> */}
       <Head />
       <body>
         <ColorModeScript />
