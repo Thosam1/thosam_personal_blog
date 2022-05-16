ddd94df9e653519012c1a17c1aa6b2a2b049fa9e
Thosam1 <thosamtenzin2002@gmail.com>
2022-05-16 23:13:50 +0200

    trying out dom animation and lazy motion in FadeInWrapper.js

--- components/FadeInWrapper.js
+++ components/FadeInWrapper.js
@@ -1,19 +1,23 @@
-import { motion } from "framer-motion"
+import { motion, LazyMotion, domAnimation } from "framer-motion";
 
 // wrapper for fade in animation
-export const FadeInWrapper = ({children}) => {
-    return (<motion.div
-      initial="hidden"
-      whileInView="visible"
-      viewport={{ once: true }}
-      transition={{ duration: 0.8 }}
-      variants={{
-        visible: { opacity: 1, scale: 1 },
-        hidden: { opacity: 0, scale: 1 },
-      }}
-    >
-      {children}
-    </motion.div>)
-  };
+export const FadeInWrapper = ({ children }) => {
+  return (
+    <LazyMotion features={domAnimation}>
+      <motion.div
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true }}
+        transition={{ duration: 0.8 }}
+        variants={{
+          visible: { opacity: 1, scale: 1 },
+          hidden: { opacity: 0, scale: 1 },
+        }}
+      >
+        {children}
+      </motion.div>
+    </LazyMotion>
+  );
+};
 
-  export default FadeInWrapper;
+export default FadeInWrapper;
