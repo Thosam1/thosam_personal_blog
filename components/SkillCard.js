0c481f6d713acc3ca7892b3a79f0db8fe208385f
Thosam1 <thosamtenzin2002@gmail.com>
2022-05-17 02:09:05 +0200

    trying to make size work

--- components/SkillCard.js
+++ components/SkillCard.js
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import { VStack, HStack, Text, useColorModeValue, Box } from "@chakra-ui/react";
 
+import { ChakraNextImage } from "./ChakraNextImage";
+
 import { MotionBox } from "./MotionBox";
 
 const SkillCard = ({ name, image, link, description }) => {
@@ -21,7 +23,7 @@ const SkillCard = ({ name, image, link, description }) => {
           rounded="lg"
           p={2}
           position="relative"
-          overflow="hidden"
+          // overflow="hidden"
           lineHeight={0}
           boxShadow="inset 0 0 1px 1px rgba(0, 0, 0, 0.015)"
         >
@@ -34,7 +36,22 @@ const SkillCard = ({ name, image, link, description }) => {
             right={0}
             opacity={0.25}
           ></Box>
-          <Image alt={image} src={image} height={26} width={26} h={26} w={26} style />
+          {/* <ChakraNextImage 
+                    src={image}
+                    alt={image}
+                    h={26} w={26}
+                    objectFit="cover"
+          /> */}
+          <Box position="relative" height={26} width={26}>
+            <Image
+              alt={image}
+              src={image}
+              fill
+              // objectFit="cover"
+              layout={"fill"}
+              style={{ borderRadius: 5 }}
+            />
+          </Box>
         </Box>
         <VStack
           align="start"
@@ -42,6 +59,7 @@ const SkillCard = ({ name, image, link, description }) => {
           spacing={1}
           maxW="lg"
           h="100%"
+          ml={4}
         >
           <VStack spacing={0} align="start" flexGrow={1}>
             <Text
