5d37ee096eb178a7c5f36da151968059bafaa2b2
Thosam1 <thosamtenzin2002@gmail.com>
2022-09-11 22:20:24 +0200

    dark theme by default

--- pages/_app.js
+++ pages/_app.js
@@ -67,7 +67,7 @@ function MyApp({ Component, pageProps }) {
         <ColorModeProvider
           options={{
             initialColorMode: "dark",
-            useSystemColorMode: true,
+            useSystemColorMode: false,
           }}
         >
           <GlobalStyle>
